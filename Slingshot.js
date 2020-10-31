452141e17b770f806976e1d430b4ed211cd83118
Mithib2010 <Mithib2010@hotmail.com>
2020-10-30 20:04:37 -0700

    Made the rubber and the Catapolt

--- Slingshot.js
+++ Slingshot.js
@@ -9,6 +9,9 @@ class SlingShot{
         this.pointB = pointB
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
+        this.sling1=loadImage("sprites/sling1.png");
+        this.sling2=loadImage("sprites/sling2.png");
+        this.sling3=loadImage("sprites/sling3.png");
     }
 
     fly(){
@@ -16,11 +19,24 @@ class SlingShot{
     }
 
     display(){
+        image(this.sling2,220,50);
+        image(this.sling1,250,50);
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            stroke(48,22,8);
+            if(pointA.x<240){  
+                strokeWeight(7);
+                line(pointA.x-25,pointA.y,pointB.x+25,pointB.y+10);
+                line(pointA.x-25, pointA.y, pointB.x+80, pointB.y+10);
+                image(this.sling3,pointA.x-30,pointA.y-10,15,30)}
+            
+                else{  
+                strokeWeight(4);
+                line(pointA.x+25,pointA.y,pointB.x+25,pointB.y+10);
+                line(pointA.x+25, pointA.y, pointB.x+80, pointB.y+10);
+                image(this.sling3,pointA.x+25,pointA.y-10,15,30);
+            }
         }
     }
     
